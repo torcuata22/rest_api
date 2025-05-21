b48c789f70db6b72f53b98814fcc8eb809145bf8
torcuata22 <marilynjmarquez@gmail.com>
2025-05-21 11:53:00 -0400

    grabbing user id from user data working

--- routes/events.go
+++ routes/events.go
@@ -35,7 +35,7 @@ func createEvent(context *gin.Context) {
 	token = token[len(prefix):]
 
 	// Verify token
-	err := utils.VerifyToken(token)
+	userId, err := utils.VerifyToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "error": err.Error()})
 		return
@@ -48,8 +48,8 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
+	// event.ID = 1 generated when saving, so not needed
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
--- utils/jwt.go
+++ utils/jwt.go
@@ -18,7 +18,7 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (any, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 		if !ok {
@@ -27,24 +27,21 @@ func VerifyToken(token string) error {
 		return []byte(secretKey), nil //before it was secretKey, but it needs to be a byte slice: []byte(secretKey)
 	})
 	if err != nil {
-		return errors.New("could not parse token")
+		return 0, errors.New("could not parse token")
 	}
 
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
-		return errors.New("invalid token")
+		return 0, errors.New("invalid token")
+	}
+	//claims: data stored in token, including user is
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("invalid claims")
 	}
 
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
-	// if !ok {
-	// 	return errors.New("invalid claims")
-	// }
-
 	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
-	// if email == "" || userId == 0 {
-	// 	return errors.New("invalid claims")
-	// }
+	userId := int64(claims["userId"].(float64))
 
-	return nil
+	return userId, nil
 }
